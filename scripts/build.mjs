8d27ba78a965320eef16c5a2ca7c2c06a152da09
Mohammed S. Yaseen <51242349+mohasarc@users.noreply.github.com>
2024-01-17 13:12:34 +0300

    chore: add support for electron 27.0.0

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -3,7 +3,7 @@
 const archs = process.platform === 'win32' ? ['x64'] : ['x64', 'arm64'];
 
 const nodeVersions = ['16.0.0', '17.0.1', '18.0.0'];
-const electronVersions = ['19.0.0', '20.0.0', '21.0.0', '22.0.0'];
+const electronVersions = ['19.0.0', '20.0.0', '21.0.0', '22.0.0', '27.0.0'];
 
 for (const arch of archs) {
     await $`npx prebuildify --strip --arch ${arch} ${nodeVersions.map(v => ['-t', `node@${v}`]).flat()} ${electronVersions.map(v => ['-t', `electron@${v}`]).flat()}`;
