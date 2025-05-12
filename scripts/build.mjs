56f6815c8e6dc5f17b287745b5412f4e84be02c1
zeyysu <zeynepsuylcn@gmail.com>
2025-05-12 17:35:42 +0300

    chore: build for newer packages

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -2,9 +2,14 @@
 
 const archs = process.platform === 'win32' ? ['x64'] : ['x64', 'arm64'];
 
-const nodeVersions = ['16.0.0', '17.0.1', '18.0.0'];
-const electronVersions = ['19.0.0', '20.0.0', '21.0.0', '22.0.0', '27.0.0', '31.0.0'];
+const nodeVersions = ['18.0.0', '19.0.0', '20.0.0', '21.0.0', '22.0.0'];
+const electronVersions = ['22.0.0', '27.0.0', '31.0.0', '32.0.0', '33.0.0', '34.0.0'];
 
 for (const arch of archs) {
-    await $`npx prebuildify --strip --arch ${arch} ${nodeVersions.map(v => ['-t', `node@${v}`]).flat()} ${electronVersions.map(v => ['-t', `electron@${v}`]).flat()}`;
+    try {
+        await $`npx prebuildify --strip --arch ${arch} ${nodeVersions.map(v => ['-t', `node@${v}`]).flat()} ${electronVersions.map(v => ['-t', `electron@${v}`]).flat()}`;
+    }
+    catch (error) {
+        console.error(`Error building for architecture ${arch}:`, error);
+    }
 }
